589496d3187e10df1c67c739fb7701f9cd54a8f8
Rohit Kumar <123821699+rohitt24k@users.noreply.github.com>
2024-07-20 14:29:55 +0530

    Time: 12 ms (39.72%) | Memory: 16.7 MB (27.62%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -4,39 +4,44 @@ public:
         int row = matrix.size();
         int col = matrix[0].size();
 
-        int rows[row];
-        int cols[col];
-
-        for(int i = 0; i < row; i++){
-            rows[i] = 1;
-        }
-        for(int i = 0; i < col; i++){
-            cols[i] = 1;
-        }
+        int common = matrix[0][0];
 
         for(int i = 0; i < row; i++){
             for(int j = 0; j < col; j++){
                 if(matrix[i][j] == 0){
-                    rows[i] = 0;
-                    cols[j] = 0;
+                    if(j != 0){
+                        matrix[0][j] = 0;
+                    }else{
+                        common = 0;
+                    }
+                    matrix[i][0] = 0;
                 }
             }
         }
+
+        for(int j = 1; j < col; j++){
+            if(matrix[0][j] == 0){
+                for(int i = 1; i < row; i++){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        
 
         for(int i = 0; i < row; i++){
-            if(rows[i] == 0){
-                for(int j = 0; j < col; j++){
+            if(matrix[i][0] == 0){
+                for(int j = 1; j < col; j++){
                     matrix[i][j] = 0;
                 }
             }
         }
 
-        for(int j = 0; j < col; j++){
-            if(cols[j] == 0){
-                for(int i = 0; i < row; i++){
-                    matrix[i][j] = 0;
-                }
+        if(common == 0){
+            for(int i = 0; i < row; i++){
+                matrix[i][0] = 0;
             }
         }
+        
     }
 };
