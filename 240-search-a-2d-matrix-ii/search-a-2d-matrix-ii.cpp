235a0bbc96d08956276eb74236863c4c732f2f63
Rohit Kumar <123821699+rohitt24k@users.noreply.github.com>
2024-10-18 01:27:35 +0530

    Time: 43 ms (71.76%) | Memory: 17.7 MB (44.07%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -6,22 +6,16 @@ public:
         int x = matrix.size();
         int y = matrix[0].size();
 
-        for(int v = y-1; v >= 0; v--){
-            if(target < matrix[0][v]) continue;
+        int u = 0;
+        int v = y - 1;
 
-            int s = 0;
-            int e = x - 1;
-
-            while(s <= e){
-                int m = s + (e - s) / 2;
-
-                if(matrix[m][v] == target){
-                    return true;
-                }else if(matrix[m][v] > target){
-                    e = m - 1;
-                }else{
-                    s = m + 1;
-                }
+        while( v >= 0 && u < x){
+            if(matrix[u][v] == target){
+                return true;
+            }else if(matrix[u][v] < target){
+                u++;
+            }else{
+                v--;
             }
         }
         return false;
