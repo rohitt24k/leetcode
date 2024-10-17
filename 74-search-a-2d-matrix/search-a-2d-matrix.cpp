77fdea1fd34d2fc055c1e4cc62d5d29c81458ac0
Rohit Kumar <123821699+rohitt24k@users.noreply.github.com>
2024-10-17 18:32:40 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (92.30%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -4,22 +4,42 @@ public:
         int x = matrix.size();
         int y = matrix[0].size();
         
-        int low = 0;
-        int high = x * y - 1;
+        // first find in which row
 
-        while(low <= high){
-            int m = low + (high - low) / 2;
+        int s = 0;
+        int e = x - 1;
 
-            int u = m / y;
-            int v = m % y;
-            if(matrix[u][v] == target){
+        
+        while(s<=e){
+            int m = s + (e - s) / 2;
+
+            if(matrix[m][0] == target){
                 return true;
-            }else if(matrix[u][v] > target){
-                high = m - 1;
+            }else if(matrix[m][0] > target){
+                e = m - 1;
             }else{
-                low = m + 1;
+                s = m + 1;
             }
-        }  
+        }
+
+        int u = e;
+
+        if(u < 0 || u >= x) return false;
+
+        s = 0;
+        e = y - 1;
+
+        while(s <= e){
+            int m = s + (e - s) / 2;
+
+            if(matrix[u][m] == target){
+                return true;
+            }else if(matrix[u][m] > target){
+                e = m - 1;
+            }else{
+                s = m + 1;
+            }
+        }
 
         return false;      
     }
