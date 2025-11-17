98d7f01e2f30a584a74ddfe760c8618f6e8ee1a8
Rohit Kumar <123821699+rohitt24k@users.noreply.github.com>
2025-11-17 14:08:41 +0530

    Time: 2 ms (40.35%) | Memory: 12.4 MB (5.94%) - LeetSync

--- 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.cpp
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
-    static constexpr int P = int(1E9) + 7;
-
     int numSub(string s) {
-        int p = 0;
-        long long ans = 0;
-        while (p < s.size()) {
-            if (s[p] == '0') {
-                ++p;
-                continue;
-            }
-            int cnt = 0;
-            while (p < s.size() && s[p] == '1') {
-                ++cnt;
-                ++p;
-            }
-            ans = ans + (1LL + (long long)cnt) * cnt / 2;
-            ans = ans % P;
+        queue<int> zeroPosition;
+
+        zeroPosition.push(-1);
+
+        for(int i = 0; i < s.size(); i++){
+            char c = s[i];
+            if(c == '0') zeroPosition.push(i);
         }
+
+        zeroPosition.push(s.size());
+
+        int prev = zeroPosition.front();
+        zeroPosition.pop();
+
+        int ans = 0;
+        int MAXI = 1000000007;
+
+        while(!zeroPosition.empty()){
+            int curr = zeroPosition.front();
+            zeroPosition.pop();
+
+            long long n = curr - prev - 1;
+            int currAns = (n * (n + 1) / 2) % MAXI;
+
+            currAns = max(0, currAns);
+            
+            // cout<<"checking for pos="<<curr<<" currAns="<<currAns<<endl;
+            ans = (ans + currAns) % MAXI;
+            prev = curr;
+        }
+
         return ans;
     }
 };
